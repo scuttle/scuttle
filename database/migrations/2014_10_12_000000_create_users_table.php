d548f22ece0bcb2c9b4d7f8cf54c05f29a2456fc
bluesoul <github@nags.me>
2018-07-18 14:54:46 -0600

    Add preferences and roles json fields.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->json('preferences');
+            $table->json('roles');
             $table->rememberToken();
             $table->timestamps();
         });
