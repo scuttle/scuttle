57b99d7e1a2f3dcb26199f7d879ae45b5e89f932
bluesoul <github@nags.me>
2019-09-23 15:26:19 -0600

    API: Add token scopes.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,28 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        /**
+         * Lay out some scopes that API tokens can have. Simple verb-noun grammar.
+         */
+        Passport::tokensCan([
+            'read-metadata' => 'Read basic metadata about articles and users',
+            'read-article' => 'Get the latest revision of an article and associated metadata',
+            'read-revision' => 'Get a specific revision and associated metadata',
+            'read-votes' => 'Get a list of votes, by user or by article',
+            'read-post' => 'Get a specific forum post and all revisions',
+            'read-thread' => 'Get an entire forum thread',
+            'write-metadata' => 'Update metadata about articles and users',
+            'write-revision' => 'Commit a revision to the SCUTTLE DB',
+            'write-votes' => 'Update votes by user or article',
+            'write-post' => 'Create or update a forum post',
+            'write-thread' => 'Create or update thread metadata.'
+        ]);
+
+        Passport::setDefaultScope([
+           'read-metadata',
+           'read-votes'
+        ]);
+
         Passport::routes();
     }
 }
