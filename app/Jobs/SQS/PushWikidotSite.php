b166280146d16190d633efced16b6e4054b8cc1f
bluesoul <github@nags.me>
2019-11-14 15:21:39 -0700

    First of the new SQS connectors

---
+++ app/Jobs/SQS/PushWikidotSite.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Jobs\SQS;
+
+use Aws\Sqs\SqsClient;
+use Aws\Exception\AwsException;
+use App\Wiki;
+
+class PushWikidotSite {
+
+    public $callback_url;
+    public $wd_site;
+
+    public function __construct(int $wiki_id)
+    {
+        $wiki = Wiki::find($wiki_id);
+        $domain = $wiki->domains->pluck('domain')->first();
+        $this->callback_url = 'https://' . $domain . '/api';
+        $metadata = json_decode($wiki->metadata, true);
+        $this->wd_site = $metadata["wd_site"];
+    }
+
+    public function send(string $queue)
+    {
+        $client = new SqsClient([
+            'region' => env('SQS_REGION'),
+            'version' => '2012-11-05',
+            'credentials' => [
+                'key' => env('SQS_KEY'),
+                'secret' => env('SQS_SECRET')
+            ]
+        ]);
+
+        $params = [
+            'DelaySeconds' => 0,
+            'MessageAttributes' =>  [
+                'wikidot_site' => [
+                    'DataType' => 'String',
+                    'StringValue' => $this->wd_site
+                ],
+                'callback_url' => [
+                    'DataType' => 'String',
+                    'StringValue' => $this->callback_url
+                ]
+            ],
+            'MessageBody' => 'test',
+            'QueueUrl' => env('SQS_PREFIX') . '/' . $queue
+        ];
+
+        try {
+            $result = $client->sendMessage($params);
+        } catch (AwsException $e) {
+            // output error message if fails
+            error_log($e->getMessage());
+        }
+
+    }
+}
