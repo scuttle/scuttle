b54269c33ba1f928c8ab37173e3113750c8c08c3
bluesoul <daniel.tharp@gmail.com>
2019-12-30 18:48:59 -0700

    Pull Domain up into use.

--- app/Jobs/SQS/PushWikidotSite.php
+++ app/Jobs/SQS/PushWikidotSite.php
@@ -3,6 +3,7 @@
 
 namespace App\Jobs\SQS;
 
+use App\Domain;
 use Aws\Sqs\SqsClient;
 use Aws\Exception\AwsException;
 use App\Wiki;
@@ -16,7 +17,7 @@ class PushWikidotSite {
     public function __construct(int $wiki_id)
     {
         $wiki = Wiki::find($wiki_id);
-        $domain = App\Domain::where('wiki_id',$wiki_id)->where('metadata->callback',true)->pluck('domain')->first();
+        $domain = Domain::where('wiki_id',$wiki_id)->where('metadata->callback',true)->pluck('domain')->first();
         $this->callback_url = 'https://' . $domain . '/api';
         $metadata = json_decode($wiki->metadata, true);
         $this->wd_site = $metadata["wd_site"];
