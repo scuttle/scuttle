c4d63d40153218625b29d4967a5436a9efbc487b
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-06 19:40:30 -0500

    Split job.

--- app/helpers.php
+++ app/helpers.php
@@ -4,8 +4,7 @@ use App\Notifications\PostJobStatusToDiscord;
 use Illuminate\Support\Facades\Notification;
 
 function discord($message): void {
-    Notification::route('discord', env('DISCORD_BOT_CHANNEL'))->notify(new PostJobStatusToDiscord(
-        $message
-    ));
+    $job = new PostJobStatusToDiscord($message);
+    Notification::route('discord', env('DISCORD_BOT_CHANNEL'))->notify($job);
     return;
 }
