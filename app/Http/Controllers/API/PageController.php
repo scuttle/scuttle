88f7d8237e8091b42d0a4e03c827ef3f52260071
bluesoul <daniel.tharp@gmail.com>
2020-02-29 13:03:33 -0700

    Use Page object rather than collection.

--- app/Http/Controllers/API/PageController.php
+++ app/Http/Controllers/API/PageController.php
@@ -244,8 +244,9 @@ class PageController extends Controller
     {
         if(Gate::allows('write-programmatically')) {
             // Our page may have been renamed rather than a brand new one. Let's quickly check for that.
-            $page = Page::withTrashed()->where('wd_page_id', $request['wd_page_id'])->get();
-            if ($page->isNotEmpty()) {
+            $p = Page::withTrashed()->where('wd_page_id', $request['wd_page_id'])->get();
+            if ($p->isNotEmpty()) {
+                $page = $p->first();
                 // Renamed page, let's do the thing.
                 // First off, there's apparently some pages that were incorrectly soft-deleted. Undelete them if so.
                 if($page->trashed()) {
