f9e9d95f27ad1d684fac04dbeb67deb03351a809
bluesoul <atlassian@nags.me>
2020-04-15 19:16:37 -0600

    PageTag object

---
+++ app/Http/Controllers/PageTagController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PageTag;
+use Illuminate\Http\Request;
+
+class PageTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PageTag  $pageTag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PageTag $pageTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PageTag  $pageTag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PageTag $pageTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PageTag  $pageTag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PageTag $pageTag)
+    {
+        //
+    }
+}
